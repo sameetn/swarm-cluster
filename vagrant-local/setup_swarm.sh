726135271036d2785e689ebfcc12b65d58789170
Sameet Nasnodkar <sameet.nasnodkar@gmail.com>
2016-12-14 09:20:54 -0500

    modified to allow for remote and local swarm setups

--- vagrant-local/setup_swarm.sh
+++ vagrant-local/setup_swarm.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-swarm_nodes="swarm-node-1 swarm-node-2"
+swarm_nodes="swarm-node-1-local swarm-node-2-local"
 
 echo "Creating the master"
-docker-machine create -d virtualbox swarm-master
+docker-machine create -d virtualbox swarm-master-local
 master_ip=$(docker-machine ip swarm-master)
 
 echo "Initializing the swarm mode"
@@ -11,6 +11,9 @@ docker-machine ssh swarm-master docker swarm init --advertise-addr $master_ip
 
 join_token=$(docker-machine ssh swarm-master docker swarm join-token -q worker)
 echo "Join token is ${join_token}"
+
+# Setup the portainer management
+docker run -d -p 9000:9000 portainer/portainer -H tcp://$master_ip:2377 --swarm
 
 # Create and join the workers
 for worker in $swarm_nodes; do
